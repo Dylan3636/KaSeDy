546e7d39825faa7c71593de108b3b1ba45b1b89e
Dylan Angus <dylanangus@hotmail.com>
2017-07-03 22:00:35 -0400

    added test code to arduino

--- pybot/ardI2C.py
+++ pybot/ardI2C.py
@@ -1,11 +1,16 @@
 import smbus
-
-
+import time
 class Arduino():
     def __init__(self,address=0x04,i2c_bus = 1):
-        self.adress = address
+        self.address = address
         self.bus = smbus.SMBus(1)
     def talk(self,message):
+        self.bus.write_byte(self.address,message )
 
-        self.bus.write.data()
-
+    def listen(self):
+        return self.bus.read_byte(self.address)
+def main():
+    ard = Arduino()
+    while True:
+        ard.talk(1)
+        time.sleep(0.5)
