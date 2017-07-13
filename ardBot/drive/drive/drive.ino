f9f92cc89a278f7b8e7230cda940c5c494cd7fe2
Dylan <dylanangus@hotmail.com>
2017-07-13 17:25:47 -0400

    adding drive

---
+++ ardBot/drive/drive/drive.ino
@@ -0,0 +1,66 @@
+#include <i2c_pi.h>
+#include <Motor_Control.h>
+
+i2c_pi pi = i2c_pi();
+Motor_Control motors = Motor_Control(1,2) ;
+bool on = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+    Wire.onReceive(receive_data);
+    while(!on){
+      on = pi.get_on();
+      delay(50);
+    }
+
+}
+
+void loop() {
+  while(on == false){
+    bool on = pi.get_on();
+  }
+
+  int* data = pi.get_data();
+  
+  if((data!=nullptr)){
+    int op = pi.get_operation();
+    if(op==0x02){
+        int action = data[1];
+
+        switch(action) {
+          case 0: motors.halt();break;
+          case 1: motors.forward_forever(150); break;
+          case 2: motors.turn_anticlockwise_forever(150); break;
+          case 3: motors.turn_clockwise_forever(150); break;
+          case 4: motors.backward_forever(150); break;
+    }
+
+    }
+  }
+  
+}
+void receive_data(int byte_count){
+    int * data = pi.get_data();
+    if(data !=  nullptr){
+        delete data;
+        data = nullptr;
+    }
+    data = new int[byte_count];
+    pi.set_operation(Wire.read()); // internal address read from Wire
+
+    if (pi.get_operation() == 0x00)
+        pi.set_on(Wire.read()==1);          //Turn Arduino on/off
+    else{
+        int pos = (sizeof( data ) / sizeof( data[0] ));
+       // if(pos <= byte_count)
+       //    data[pos] = operation;
+        int i = 0;
+        while(Wire.available()){
+            if(i <= byte_count)
+                data[i++] = Wire.read();
+        }
+    }
+    pi.set_data(data);
+};
+
