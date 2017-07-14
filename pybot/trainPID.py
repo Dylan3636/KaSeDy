9d07811543701a5807a9386b683177fbfcd7271c
Dylan <dylanangus@hotmail.com>
2017-07-14 15:11:01 -0400

    c

--- pybot/trainPID.py
+++ pybot/trainPID.py
@@ -1,10 +1,9 @@
 from ardI2C import Arduino
 ard = Arduino()
-import numpy as np
-import click
+
 exit = 0
 ard.talk(1)
-while(exit == 0):
+while exit == 0:
     value = raw_input('Enter PID gains (q to quit): ')
     if value == 'q':
         exit = 1
